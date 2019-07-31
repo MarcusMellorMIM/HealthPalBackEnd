834d70b1a52bd5c7573d98e2d21e9b8c02665551
MarcusMellorMIM <marcus@mellor.com>
2019-07-31 10:35:05 +0100

    Added email to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_30_133908) do
+ActiveRecord::Schema.define(version: 2019_07_31_093329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2019_07_30_133908) do
     t.string "gender"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
   create_table "weights", force: :cascade do |t|
